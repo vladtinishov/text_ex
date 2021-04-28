7e8425662b82675899d529ee18b10b336c4aaaaf
Vladislav <vladislav04022001@gmail.com>
2021-04-28 22:02:50 +0600

    немного подправил php код

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,31 +5,41 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Products;
 use Illuminate\Support\Facades\DB;
+
 class ProductsController extends Controller
 {
-    public function showAll(){ 
+    public function showAll()
+    { 
         $allProducts = Products::getAll();
         return json_encode($allProducts);
+
     }
-    public function search(Request $request){
+    public function search(Request $request)
+    {
         $gotObj = json_decode($request->input('sql'));
-        $sqlFull = 'select * from products where ';
+        $sqlFull = 'SELECT * FROM products WHERE ';
         $sql = '';
-        foreach($gotObj as $key => $value){
+
+        foreach ($gotObj as $key => $value) {
+
             $val = $value->val;
             $exp = $value->exp;
-            if($value->val != ''){
-                if($key == 'id' || $key == 'price' || $key == 'weight'){
+            
+            if ($value->val != '') {
+                if ($key == 'id' || $key == 'price' || $key == 'weight'){
                     $sql .= "$key $exp $val AND ";
-                }
-                else{
+                } else {
                     $sql .= "$key $exp '$val' AND ";
                 }
+
             }
         }
+
         if($sql == '') return '';  
+
         $sqlFull .= substr($sql, 0, -4);
         $result = DB::select($sqlFull);
+
         return json_encode($result);
     }
 }
